108f120adf083e679bbdcde2e1587cc90087af77
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-04-13 19:07:20 -0700

    added private params, built the new image for the description as a required arg

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,20 @@
 class ImagesController < ApplicationController
+    validates :description, presence: true
+    
+    def index
+    end
+
+    def show
+    end
+
+    def new
+        @gallery = Gallery.find(params[:gallery_id]) # Find the parent gallery
+        @image = @gallery.images.new # Build a new image associated with the gallery
+    end
+
+    private
+
+    def image_params
+        params.require(:image).permit(:author, :width, :height, :url, :download_url, :description).merge(gallery_id: params[:gallery_id])
+    end
 end
